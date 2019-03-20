9e1bdd749eed391c13ab0254b337d08103c30319
Leonardo <leo01_alves@hotmail.com>
2019-03-20 11:17:55 -0300

    :sparkles: export our own themes and mixins to provide it to library users

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import Button from './components/Button';
 import Loader from './components/Loader';
 import './fonts.css';
+import styles from './styles/variables';
+import * as mixins from './styles/mixins';
+
+const theme = {
+  styles,
+  mixins,
+};
 
 export {
   Button,
   Loader,
+  theme,
 };
