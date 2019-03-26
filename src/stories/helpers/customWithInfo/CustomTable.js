809486a6f85e587e77de3dd06dd781c4a35ae841
Leonardo <leo01_alves@hotmail.com>
2019-03-26 14:56:33 -0300

    :art: improve stories info customization and add customInfo addon to stories of Input and InputCurrency

--- src/stories/helpers/customWithInfo/CustomTable.js
+++ src/stories/helpers/customWithInfo/CustomTable.js
@@ -1,63 +1,71 @@
 import React, { Fragment } from 'react';
-import styled from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
 import styles from '../../../styles/variables';
 import { GlobalStyle } from '../../../styles/mixins';
 
 
 const CustomTable = styled.table`
-border-collapse: collapse;
-width: 100%;
-max-width: 500px;
-margin: 20px 0px 10px;
-border-left: 4px solid ${styles.colors.MAIN_COLOR};
-th, td {
-  padding: 3px 5px;
-  border-bottom: 1px solid ${styles.colors.GRAY_LIGHT_2};
-  color: ${styles.colors.GRAY_DARKER};
-}
-th {
-  text-align: left;
-  border-bottom: 2px solid ${styles.colors.GRAY_LIGHT_2};
-  color: ${styles.colors.GRAY_DARKER};
-}
-td {
-  font-size: 12px;
-  span {
-    margin-left: 5px;
-    color: ${styles.colors.DANGER_COLOR};
-    font-size: 14px;
-    font-weight: bold;
+  border-collapse: collapse;
+  width: 100%;
+  max-width: 500px;
+  margin: 20px 0px 10px;
+  border-left: 4px solid ${styles.colors.MAIN_COLOR};
+  th, td {
+    padding: 3px 5px;
+    border-bottom: 1px solid ${styles.colors.GRAY_LIGHT_2};
+    color: ${styles.colors.GRAY_DARKER};
   }
-}
-tr {
-  &:last-of-type {
-    td {
-      border-bottom: unset;
+  th {
+    text-align: left;
+    border-bottom: 2px solid ${styles.colors.GRAY_LIGHT_2};
+    color: ${styles.colors.GRAY_DARKER};
+  }
+  td {
+    font-size: 12px;
+    span {
+      margin-left: 5px;
+      color: ${styles.colors.DANGER_COLOR};
+      font-size: 14px;
+      font-weight: bold;
     }
   }
-}
+  tr {
+    &:last-of-type {
+      td {
+        border-bottom: unset;
+      }
+    }
+  }
 `;
 
 const PropType = styled.td`
-text-transform: capitalize;
-font-style: italic;
+  text-transform: capitalize;
+  font-style: italic;
 `;
 
 const DefaultValue = styled.td`
-font-weight: bold;
+  font-weight: bold;
 `;
 
 const Description = styled.p`
-margin-top: 10px;
-margin-bottom: 40px;
-color: ${styles.colors.DANGER_COLOR};
-font-style: italic;
-font-size: 12px;
+  margin-top: 10px;
+  margin-bottom: 40px;
+  color: ${styles.colors.DANGER_COLOR};
+  font-style: italic;
+  font-size: 12px;
+`;
+
+const InfoGlobalStyle = createGlobalStyle`
+  .info__close-button {
+    font-size: 24px !important;
+    padding: 5px 15px !important;
+  }
 `;
 
 const CustomTableProps = ({ propDefinitions }) => {
   return (
     <Fragment>
+      <InfoGlobalStyle />
       <GlobalStyle />
       <CustomTable>
         <thead>
--- src/stories/inputCurrency.js
+++ src/stories/inputCurrency.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react'; // eslint-disable-line import/no-extraneous-dependencies
 import { State, Store } from '@sambego/storybook-state'; // eslint-disable-line import/no-extraneous-dependencies
 import InputCurrency from '../components/InputCurrency';
+import customWithInfo from './helpers/customWithInfo';
 
 const store = new Store({
   primary: 0,
@@ -19,15 +20,15 @@ storiesOf('InputCurrency', module)
       {state => (storyFn(state))}
     </State>
   ))
-  .add('Primary style', state => (
+  .add('Primary style', customWithInfo()(state => (
     <InputCurrency
       name="primary"
       onChange={handleChange}
       value={state.primary}
       placeholder="Primary style"
     />
-  ))
-  .add('Disabled style', state => (
+  )))
+  .add('Disabled style', customWithInfo()(state => (
     <InputCurrency
       name="disabled"
       onChange={handleChange}
@@ -35,8 +36,8 @@ storiesOf('InputCurrency', module)
       placeholder="Disabled style"
       disabled
     />
-  ))
-  .add('Error style', state => (
+  )))
+  .add('Error style', customWithInfo()(state => (
     <InputCurrency
       name="error"
       onChange={handleChange}
@@ -44,8 +45,8 @@ storiesOf('InputCurrency', module)
       placeholder="Error style"
       error
     />
-  ))
-  .add('Checked style', state => (
+  )))
+  .add('Checked style', customWithInfo()(state => (
     <InputCurrency
       name="checked"
       onChange={handleChange}
@@ -53,8 +54,8 @@ storiesOf('InputCurrency', module)
       placeholder="Checked style"
       checked
     />
-  ))
-  .add('Disabled checked style', state => (
+  )))
+  .add('Disabled checked style', customWithInfo()(state => (
     <InputCurrency
       name="disabledChecked"
       onChange={handleChange}
@@ -63,8 +64,8 @@ storiesOf('InputCurrency', module)
       disabled
       checked
     />
-  ))
-  .add('Custom style', state => (
+  )))
+  .add('Custom style', customWithInfo()(state => (
     <InputCurrency
       name="custom"
       onChange={handleChange}
@@ -76,4 +77,4 @@ storiesOf('InputCurrency', module)
         border: '1px dashed #118473',
       }}
     />
-  ));
+  )));
