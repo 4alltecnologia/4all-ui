30ed21d9b9fac4549a9288e324115fe60fe8cefe
Eduardo Pieretti Umpierre <eduardo.umpierre@4all.com>
2019-03-26 11:21:36 -0300

    :lipstick: border variable updated and disabled style fixed

--- src/components/Input/style.js
+++ src/components/Input/style.js
@@ -3,7 +3,7 @@ import { fontStyle } from '../../styles/mixins';
 import theme from '../../styles/variables';
 
 export const Input = styled.input`
-  border: 1px solid ${({ error }) => error ? theme.colors.DANGER_COLOR : theme.colors.GRAY_MEDIUM_2};
+  border: 1px solid ${({ error }) => error ? theme.colors.DANGER_COLOR : theme.colors.GRAY_LIGHT_2};
   border-radius: 3px;
   ${fontStyle(
     theme.colors.GRAY_DARKER,
@@ -20,6 +20,10 @@ export const Input = styled.input`
 
   ::placeholder {
     color: ${theme.colors.GRAY_MEDIUM}
+  }
+
+  :disabled {
+    background-color: ${theme.colors.GRAY_LIGHT_2};
   }
 `;
 
