12383febed10d5b162fc4cc4e515a3fb43bd5ba1
Mathias <mathias.falci@gmail.com>
2019-05-20 15:45:55 -0300

    :sparkles: does not change input value on typing letters and improve input funcionality

--- src/components/InputCurrency/index.js
+++ src/components/InputCurrency/index.js
@@ -1,41 +1,79 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Input from '../Input';
 import {
   formatPrice,
   priceToInt,
   getFormattedValue,
+  adjustCaretPosition,
+  getTargetInputParams,
 } from '../../helpers/price';
 
-const InputCurrency = ({
-  id,
-  name,
-  type,
-  value,
-  onChange,
-  placeholder,
-  disabled,
-  error,
-  customStyles,
-  checked,
-  className,
-  width,
-}) => (
-  <Input
-    id={id}
-    name={name}
-    type={type}
-    value={formatPrice(priceToInt(value) / 100)}
-    onChange={e => onChange(e, getFormattedValue(e))}
-    placeholder={placeholder}
-    disabled={disabled}
-    error={error}
-    customStyles={customStyles}
-    checked={checked}
-    className={`input-currency-4all ${className}`}
-    width={width}
-  />
-);
+export default class InputCurrency extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      caretPosition: null,
+      event: null,
+    };
+  }
+
+  componentDidUpdate(prevProps) {
+    const value = this.props;
+    const { caretPosition, event } = this.state;
+    if (prevProps.value !== value && !!caretPosition) {
+      event.target.setSelectionRange(caretPosition, caretPosition);
+    }
+  }
+
+  onChangePrice = (e, onChange) => {
+    e.persist();
+    const targetInputParams = getTargetInputParams(e);
+    const hasLetters = e.target.value.match(/[a-z]/i);
+    const formattedValue = getFormattedValue(e);
+    if (!hasLetters) {
+      this.setState({
+        event: e,
+        caretPosition: adjustCaretPosition(targetInputParams, formattedValue),
+      });
+    }
+
+    return onChange(e, formattedValue);
+  };
+
+  render() {
+    const {
+      id,
+      name,
+      type,
+      value,
+      onChange,
+      placeholder,
+      disabled,
+      error,
+      customStyles,
+      checked,
+      className,
+      width,
+    } = this.props;
+    return (
+      <Input
+        id={id}
+        name={name}
+        type={type}
+        value={formatPrice(priceToInt(value) / 100)}
+        onChange={e => this.onChangePrice(e, onChange)}
+        placeholder={placeholder}
+        disabled={disabled}
+        error={error}
+        customStyles={customStyles}
+        checked={checked}
+        className={`input-currency-4all ${className}`}
+        width={width}
+      />
+    );
+  }
+}
 
 InputCurrency.propTypes = {
   id: PropTypes.string,
@@ -65,5 +103,3 @@ InputCurrency.defaultProps = {
   className: '',
   width: '100%',
 };
-
-export default InputCurrency;
