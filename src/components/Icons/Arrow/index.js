ed2020f9302e3ba2825778ccfa24e0cae6c3676c
Anny Figueira <anny.figueira@somosphi.com>
2020-02-04 11:12:28 -0300

    :bug: fix arrow icon id

--- src/components/Icons/Arrow/index.js
+++ src/components/Icons/Arrow/index.js
@@ -42,16 +42,16 @@ const ArrowComponent = ({ color, size, rotate, opacity }) => {
     >
       <defs>
         <path
-          id="a"
+          id="arrow-icon-path"
           d="M7.38 1.04l2.502 2.909a.833.833 0 01.001 1.085L7.381 7.958a.833.833 0 11-1.266-1.083l1.318-1.542H1.75a.833.833 0 110-1.666l5.69-.001-1.324-1.54A.833.833 0 017.38 1.04z"
         />
       </defs>
       <g fill="none" fillRule="evenodd" opacity={opacity} transform="translate(7 8)">
-        <mask id="b" fill="#fff">
-          <use xlinkHref="#a" />
+        <mask id="arrow-icon-mask" fill="#fff">
+          <use xlinkHref="#arrow-icon-path" />
         </mask>
-        <use fill={color} fillRule="nonzero" xlinkHref="#a" />
-        <g fill={color} mask="url(#b)">
+        <use fill={color} fillRule="nonzero" xlinkHref="#arrow-icon-path" />
+        <g fill={color} mask="url(#arrow-icon-mask)">
           <path d="M-7-8h25v25H-7z" />
         </g>
       </g>
